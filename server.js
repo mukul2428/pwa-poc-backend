a50e2935275b8146ceb0df4ded348f1dd73db6ec
Mukul Raghav <mukulraghav28@gmail.com>
2024-05-16 16:42:24 +0530

    feat: backend created

---
+++ server.js
@@ -0,0 +1,62 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const PushNotifications = require("node-pushnotifications");
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(function (req, res, next) {
+  // Website you wish to allow to connect
+  res.setHeader("Access-Control-Allow-Origin", "*");
+
+  // Request methods you wish to allow
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+  );
+
+  // Request headers you wish to allow
+  res.setHeader("Access-Control-Allow-Headers", "*");
+
+  // Set to true if you need the website to include cookies in the requests sent
+  // to the API (e.g. in case you use sessions)
+  res.setHeader("Access-Control-Allow-Credentials", true);
+
+  // Pass to next layer of middleware
+  next();
+});
+
+app.post("/subscribe", (req, res) => {
+  // Get pushSubscription object
+  const subscription = req.body;
+  const settings = {
+    web: {
+      vapidDetails: {
+        subject: process.env.SUBJECT,
+        publicKey: process.env.VAPID_PUBLIC_KEY,
+        privateKey: process.env.VAPID_PRIVATE_KEY,
+      },
+      gcmAPIKey: "gcmkey",
+      TTL: 2419200,
+      contentEncoding: "aes128gcm",
+      headers: {},
+    },
+    isAlwaysUseFCM: false,
+  };
+
+  // Send 201 - resource created
+  const push = new PushNotifications(settings);
+
+  // Create payload
+  const payload = { title: "Notification from Knock" };
+  push.send(subscription, payload, (err, result) => {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log(result);
+    }
+  });
+});
+const port = 4000;
+
+app.listen(port, () => console.log(`Server started on port ${port}`));
